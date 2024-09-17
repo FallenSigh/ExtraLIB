ca2d480edc0f866845249c2cf45be4771508c008
FallenSigh <1352602006@qq.com>
2024-09-17 14:10:19 +0800

    optimize

--- src/details/print_tuple.h
+++ src/details/print_tuple.h
@@ -2,45 +2,29 @@
 #include <iostream>
 #include <tuple>
 #include <sstream>
+#include <utility>
 
-template <typename Tuple, std::size_t N>
-struct TupleHelper {
-    static void print(std::stringstream& ss, const Tuple& t) noexcept {
-        TupleHelper<Tuple, N - 1>::print(ss, t);
-        if constexpr (N > 1) {
-            ss << ", ";
-        }
-        ss << std::get<N - 1>(t);
-    }
-};
-
-template <typename Tuple>
-struct TupleHelper<Tuple, 0> {
-    static void print(std::stringstream&, const Tuple&) noexcept {
-    }
-};
+template<typename Tuple, std::size_t... I>
+void print_tuple(std::stringstream& ss, const Tuple& t, std::index_sequence<I...>) {
+    ss << "(";
+    bool first = true; 
+    ((ss << (first ? (first = false, "") : ", ") << std::get<I>(t)), ...);
+    ss << ")";
+}
 
 template <typename ...Types>
 std::ostream& operator<<(std::ostream& os, const std::tuple<Types...>& t) noexcept {
     std::stringstream ss;
-    ss << "(";
-    if constexpr (sizeof...(Types) > 0) {
-        TupleHelper<std::tuple<Types...>, sizeof...(Types)>::print(ss, t);
-    }
-    ss << ")";
+    print_tuple(ss, t, std::make_index_sequence<sizeof...(Types)>());
     os << ss.str();
     return os;
 }
 
 template <typename ...Types>
 struct std::formatter<std::tuple<Types...>> : std::formatter<std::string> {
-    auto format(const std::tuple<Types...>& tuple, auto& ctx) const {
+    auto format(const std::tuple<Types...>& t, auto& ctx) const {
         std::stringstream ss;
-        ss << "(";
-        if constexpr (sizeof...(Types) > 0) {
-            TupleHelper<std::tuple<Types...>, sizeof...(Types)>::print(ss, tuple);
-        }
-        ss << ")";
+        print_tuple(ss, t, std::make_index_sequence<sizeof...(Types)>());
         return std::format_to(ctx.out(), "{}", ss.str());
     }
 };
