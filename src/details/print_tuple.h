517984f6261146547d792f6cf34d4bd4b7c87e18
FallenSigh <1352602006@qq.com>
2024-09-20 00:28:50 +0800

    add print vector

--- src/details/print_tuple.h
+++ src/details/print_tuple.h
@@ -3,6 +3,7 @@
 #include <tuple>
 #include <sstream>
 #include <utility>
+#include <vector>
 
 template<typename Tuple, std::size_t... I>
 void print_tuple(std::stringstream& ss, const Tuple& t, std::index_sequence<I...>) {
@@ -25,6 +26,20 @@ struct std::formatter<std::tuple<Types...>> : std::formatter<std::string> {
     auto format(const std::tuple<Types...>& t, auto& ctx) const {
         std::stringstream ss;
         print_tuple(ss, t, std::make_index_sequence<sizeof...(Types)>());
+        return std::format_to(ctx.out(), "{}", ss.str());
+    }
+};
+
+template <typename T>
+struct std::formatter<std::vector<T>> : std::formatter<T> {
+    auto format(const auto& vec, auto& ctx) const {
+        std::stringstream ss;
+        ss << "[";
+        for (std::size_t i = 0; i < vec.size() - 1; i++) {
+            ss << vec[i] << ", ";
+        }
+        ss << vec[vec.size() - 1];
+        ss << "]";
         return std::format_to(ctx.out(), "{}", ss.str());
     }
 };
