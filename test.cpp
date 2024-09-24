01eeddea6eec06d165844cea2e8c2973b9477c0e
FallenSigh <1352602006@qq.com>
2024-09-24 23:16:01 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -13,32 +13,44 @@ int main(void) {
 {    
     // n-bit signed/unsigned integer
     nints<256> a("2147483647");
-    unints<128> b("-1");
-    std::cout << b << "\n";
-    std::cout << b + a << "\n";
+    unints<128> b("123456789");
+    std::cout << a + b << "\n";
+    std::cout << a - b << "\n";
+    std::cout << a * b << "\n";
+    std::cout << a / b << "\n\n";
 }
-    
     
 {
     // n-digits floating point
     // Not completed yet
-    nfloats<1024> f = 3.1415926;
-    std::cout << std::setprecision(10) << f << "\n";
+    nfloats<64> f = 33.1415926;
+    nfloats<64> g = 1.1616;
+    std::cout << std::setprecision(10) << f + g << "\n";
+    std::cout << std::setprecision(10) << f - g << "\n";
+    std::cout << std::setprecision(10) << f * g << "\n";
+    std::cout << std::setprecision(10) << f / g << "\n\n";
 }
 
 {
     // complex
     // Not completed yet
-    complex<int> c(1, 10);
-    complex<int> d(1, -10);
+    complex<double> c(1, 10);
+    complex<double> d(1, -10);
+    std::cout << c + d << "\n";
+    std::cout << c - d << "\n";
     std::cout << c * d << "\n";
+    std::cout << c / d << "\n\n";
 }
 
 {
     // fraction
+    // Not completed yet
     fraction<int> a(1, 3);
     fraction<int> b(1, 4);
     std::cout << a + b << "\n";
+    std::cout << a - b << "\n";
+    std::cout << a * b << "\n";
+    std::cout << a / b << "\n\n";
 }
 
 {
@@ -46,8 +58,11 @@ int main(void) {
     auto a = ones<shape<100>>();
     auto b = a.reshape<shape<5, 5, 4>>();
     ndarray<shape<2, 2>> c = {{1.14, 5.14}, {2.88, 1.46}};
-    
+    auto d = ones<shape<1, 5, 1>>();
+
     std::cout << a.slice<slice<1, 4>>() << "\n";
+    std::cout << (b + d) << "\n";
+    std::cout << (b * d) << "\n";
     std::cout << b.slice<slice<1, 2>, slice<3, 4>, void>() << "\n";
     std::cout << b.slice<void, slice<1, 2>>() << "\n";
 
